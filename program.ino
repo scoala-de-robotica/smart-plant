482a9d6786a6c99faf97c8e5ac360bf29500b16b
Alex Bordei <alex@whiz.ro>
2019-10-19 08:34:07 +0300

    Update

--- program.ino
+++ program.ino
@@ -1,106 +1,154 @@
+// Librarie folosita pentru DHT11
 #include <DHT.h>
-#define water_level D0
-#define DHTPIN D6
+
+#define soilMoistureSensor 0 // Pin senzor de umiditate in sol
+#define relay D7 // Pin releu pompa
+#define waterLevelSensor D6 // Pin senzor de nivel al apei
+#define DHTPIN D5 // Pin senzor de temperatura si umiditate
+
+// Defineste tipul senzorului folosit ("DHT11")
 #define DHTTYPE DHT11
-#define relay D7
+
+// Initiere senzor DHT11
 DHT dht(DHTPIN, DHTTYPE);
+
+//## Variabile
+int pragUmiditateSol = 60;
+int timpUdarePlanta = 1500;
+
 void setup() {
-  pinMode(relay, OUTPUT);
+  //## Setari Senzor umiditate si temperatura
+  // Pornire senzorul
   dht.begin();
-  pinMode(2, OUTPUT);
+  //##
+  //## Setari RELEU
+  // Defineste pinul relay ca fiind unul de iesire
+  pinMode(relay, OUTPUT);
+  // pentru ca este un releu activ doar daca simte semnal pe pinul de semnal, atunci aplica tensiune maxima pe pin relay
+  digitalWrite(relay, HIGH);
+  //##
+
+  //## Setari senzor nivel apa
+  //Defineste pin-ul waterLevelSensor ca fiind unul de intrare, de tip PULL UP
+  pinMode(waterLevelSensor, INPUT_PULLUP);
+  //##
+
+  //## Setari NodeMCU
+  // Initiere interfata serial
   Serial.begin(9600);
-  
-  pinMode(water_level, INPUT_PULLUP);
-  digitalWrite(water_level, LOW);
+  // Setare pin inclus ca pin de iesire
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+  //##
 }
 
-
-void blink()
-{
-  digitalWrite(2, HIGH);
-    delay(500);
-    digitalWrite(2, LOW);
-    delay(500);
-    digitalWrite(2, HIGH);
-    delay(500);
-    digitalWrite(2, LOW);
-    delay(500);
-    digitalWrite(2, HIGH);
-    delay(500);
-    digitalWrite(2, LOW);
-    delay(500);
-    digitalWrite(2, HIGH);
-    delay(500);
-    digitalWrite(2, LOW);
-    delay(500);
-    digitalWrite(2, HIGH);
-    delay(500);
-    digitalWrite(2, LOW);
-    delay(500);
-      digitalWrite(2, HIGH);
-    delay(500);
-    digitalWrite(2, LOW);
-    delay(500);
-    
-  }
-
 void loop() {
-  int val = 0;
-  int hum = 0;
-  int level = digitalRead(water_level);
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
-  
-  //DHT11
-  Serial.print("Exterior Temperature (C) = ");
-  Serial.println(t);
-  Serial.print("Exterior Humidity = ");
-  Serial.println(h);
-  delay(3000);
-  
-   Serial.print("Moisture Sensor Value:");
-  
-  hum = map(analogRead(0), 0, 1024, 0, 100);
-  
-  Serial.println(100 - hum);
-  val = 100 - hum;
+  int umiditateSol = citesteUmiditateSol();
+  bool nivelApaInVas = citesteNivelApaInVas();
+  float umiditateExterior = citesteUmiditateExterior();
+  float temperaturaExterior = citesteTempExterior();
 
-  delay(500);
-  
-  if (digitalRead(D0) == HIGH) {
-    Serial.println("am apa");
-      if (val < 20) {
-    // Planta are nevoie de apa
-    Serial.println("Planta se uda");
-    digitalWrite(relay, HIGH);
-    delay(1000);
-    digitalWrite(relay, LOW);
+  Serial.print("|Status sistem: ");
+  Serial.print("Pregatit");
+
+  Serial.print("|Umiditate in sol: ");
+  Serial.print(umiditateSol);
+
+  Serial.print("|Nivel apa in vas: ");
+  if (nivelApaInVas == HIGH) {
+    Serial.print("Da");
+  } else {
+    Serial.print("Nu");
   }
 
-  if ((val > 30) && (val < 70) ) {
-    // Planta mai vrea putina apa
-    // se va uda timp de 1.5 secunde
-    digitalWrite(relay, HIGH);
-    delay(700);
-    digitalWrite(relay, LOW);
+  Serial.print("|Umiditate in exterior: ");
+  Serial.print(umiditateExterior);
+  Serial.print(" %");
+
+
+  Serial.print("|Temperatura in exterior: ");
+  Serial.print(temperaturaExterior);
+  Serial.println(" °C");
+
+  //Daca umiditate din sol este sub pragul acceptat
+  if (umiditateSol < pragUmiditateSol) {
+    Serial.println();
+    Serial.println("Umiditate redusa. Initializez procesul de udare a plantei.");
+    //Verifica daca in vas este apa
+    if (nivelApaInVas) {
+      Serial.println("Este prezenta apa in vas. Pornire pompa");
+      // Stinge ledul se status de pe NodeMcu
+      digitalWrite(LED_BUILTIN, LOW);
+      //Porneste pompa pentru intervalul de timp de udare
+      startPompa(timpUdarePlanta);
+      Serial.println("Asteapta 1 minut.");
+      // Asteapta 1 minut ca apa sa se propage in sol
+      for (int i = 0; i < 60; i++) {
+        Serial.print("|Status sistem: ");
+        Serial.print("In asteptare");
+        Serial.print((60000 - (i * 1000)) / 1000);
+        Serial.print(" secunde");
+
+        Serial.print("|Umiditate in sol: ");
+        Serial.print(umiditateSol);
+
+        Serial.print("|Nivel apa in vas: ");
+        if (nivelApaInVas == HIGH) {
+          Serial.print("Da");
+        } else {
+          Serial.print("Nu");
+        }
+
+        Serial.print("|Umiditate in exterior: ");
+        Serial.print(umiditateExterior);
+        Serial.print(" %");
+
+
+        Serial.print("|Temperatura in exterior: ");
+        Serial.print(temperaturaExterior);
+        Serial.println(" °C");
+        delay(1000);
+      }
+    }
+    else {
+      Serial.println("Nu pot uda planta, pentru ca nu este apa in vas");
+      //Altfel porneste ledul de pe NodeMcu in semn de alerta ca in vas nu mai este apa
+      digitalWrite(LED_BUILTIN, HIGH);
+    }
   }
+  // Reia procesul odata la 5 secunde
+  delay(5000);
+}
 
-  if (val > 80) {
-    // Planta nu mai are nevoie de apa
-    // Solul este suficient de ud
-  }
+void startPompa(int timp) {
+  // Pune pompa in functiune
+  digitalWrite(relay, LOW);
 
-  }
-  
-  else {
+  // Functioneaza cat timp primeste ca parametru
+  delay(timp);
 
-    blink();
-    Serial.println("Nu am apa");
-    
-    
-  }
-  
+  // Opreste pompa din functiune
+  digitalWrite(relay, HIGH);
+}
 
-    delay(3000);
+int citesteUmiditateSol() {
+  // Citeste valoare areturnata de pin-ul analog A0 si transpune-o in intervalul 0, 100, deoarece ea este citita in intervalul 0, 1024
+  return map(analogRead(soilMoistureSensor), 0, 1024, 100, 0);
+}
 
+bool citesteNivelApaInVas() {
+  // Citeste valoarea returnata de pin-ul senzorului
+  int nivelApaInVas = digitalRead(waterLevelSensor);
+
+  //Returneaza daca valoare citita este HIGH sau LOW
+  return (nivelApaInVas == HIGH);
+
+}
+
+float citesteTempExterior() {
+  return dht.readTemperature();
+}
+
+float citesteUmiditateExterior() {
+  return dht.readHumidity();
 }
